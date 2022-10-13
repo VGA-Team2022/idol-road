c42345da35b4420c5983b51231fddc9b6feb81e5
RyoMaeda2525 <maetyuryu@gmail.com>
2022-10-13 14:07:15 +0900

    Editer上でもマウスの動きからフリックを取得できるようにした,スワイプの判定を設定しなおした

--- Assets/Maeda/ScreenInput.cs
+++ Assets/Maeda/ScreenInput.cs
@@ -6,6 +6,9 @@ using static System.Net.Mime.MediaTypeNames;
 
 public class ScreenInput : MonoBehaviour
 {
+    /// <summary>
+    /// フリックの方向やタップかを取得できる
+    /// </summary>
     public enum FlickType
     {
         None,
@@ -19,11 +22,28 @@ public class ScreenInput : MonoBehaviour
     [SerializeField, Tooltip("フリックの判定に必要な操作距離")]
     Vector2 _flickMinRange = new Vector2(0f, 0f);
 
+    [SerializeField, Tooltip("スワイプ判定に必要な時間")]
+    float _SwipeTime = 0f;
+
+    /// <summary>
+    /// 現在のフリック方向などを保存する
+    /// </summary>
     FlickType _flickType = FlickType.None;
 
+    /// <summary>
+    /// 初めに触れた、もしくはスワイプした先を保存する
+    /// </summary>
     Vector2 _StartPosition = new Vector2();
 
+    /// <summary>
+    /// 指を離した場所を保存する
+    /// </summary>
     Vector2 _EndPosition = new Vector2();
+
+    /// <summary>
+    /// スワイプを判定するためのタイマー
+    /// </summary>
+    float _timer = 0f; 
 
     void Update()
     {
@@ -38,27 +58,60 @@ public class ScreenInput : MonoBehaviour
         //Unity上でのデバック用
         if (UnityEngine.Application.isEditor)
         {
-
+            //押した、もしくはスワイプした場所を保存
+            if (Input.GetMouseButtonDown(0))
+            {
+                _StartPosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                _timer += Time.deltaTime;
+                if (_timer > _SwipeTime)
+                {
+                    _StartPosition = Input.mousePosition;
+                    _timer = 0f;
+                }
+            }
+            //離した場所を保存
+            else if (Input.GetMouseButtonUp(0))
+            {
+                _EndPosition = Input.mousePosition;
+                Frickdirection();
+            }
+            //入力がない場合はリセットする
+            else if (_flickType != FlickType.None || _flickType != FlickType.None)
+            {
+                ResetParameter();
+            }
         }
         else //スマホ用
-        {
+        {   
+            //一本以上指が触れた時
             if (Input.touchCount > 0)
             {
                 Touch touch = Input.touches[0];
 
                 switch (touch.phase)
                 {
+                    //押した、もしくはスワイプした場所を保存
                     case TouchPhase.Began:
-                    case TouchPhase.Moved:
                         _StartPosition = touch.position;
                         break;
-
+                    case TouchPhase.Moved:
+                        _timer += Time.deltaTime;
+                        if (_timer > _SwipeTime)
+                        {
+                            _StartPosition = touch.position;
+                            _timer = 0;
+                        }
+                        break;
+                    //離した場所を保存
                     case TouchPhase.Ended:
                         _EndPosition = touch.position;
                         Frickdirection();
                         break;
                 }
-            }
+            }//入力がない場合はリセットする
             else if (_flickType != FlickType.None || _flickType != FlickType.None)
             {
                 ResetParameter();
@@ -103,6 +156,7 @@ public class ScreenInput : MonoBehaviour
     private void ResetParameter()
     {
         _flickType = FlickType.None;
+        _timer = 0f;
     }
 
     public FlickType GetFlickType() 
