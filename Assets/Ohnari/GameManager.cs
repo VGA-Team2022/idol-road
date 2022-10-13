ac7b29a80bc5ad563199a0dcbd57d3acb3ecc5f4
ryooout <ryota01010104@gmail.com>
2022-10-13 22:37:39 +0900

    インスタンスの作成 倒したファンをカウントする変数、プロパティ、関数の作成 制限時間の設定（serializeFieldのほうがいいかな） #6

---
+++ Assets/Ohnari/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>ゲームマネージャーのインスタンス</summary>
+    public static GameManager instance;
+    [SerializeField]
+    [Header("倒したファンをカウント")] int _killFunAmount;
+    /// <summary>制限時間</summary>
+    float _countTime = 60;
+    /// <summary>倒したファンをカウントするプロパティ</summary>
+    public int KillFunAmount { get => _killFunAmount; set => _killFunAmount = value; }
+    /// <summary>制限時間のプロパティ</summary>
+    public float CountTime { get => _countTime; set => _countTime = value; }
+    private void Awake()
+    {
+        if(instance ==null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _countTime -= Time.deltaTime;
+        if(_countTime <=0)
+        {
+            _countTime = 0;
+        }
+    }
+    public void KillFun(int kill)
+    {
+        _killFunAmount += kill;
+    }
+}
