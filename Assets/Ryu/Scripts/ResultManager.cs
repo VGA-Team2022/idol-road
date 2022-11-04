bb7aef5f3640249fe9d85e6ba62098dee0a4d83c
RyuKento <83810925+RyuKento@users.noreply.github.com>
2022-11-04 17:13:59 +0900

    リザルト用のデータを管理するクラスの作成 #34

---
+++ Assets/Ryu/Scripts/ResultManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultManager 
+{
+    private static ResultManager _instance;
+    public ResultManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new ResultManager();
+            }
+            return _instance;
+        }
+    }
+
+    //アクションを成功度別に格納
+    private int _countBad;
+    private int _countGood;
+    private int _countGreat;
+    //ゴール地点までの距離
+    private float _distance;
+    //スーパーアイドルタイムの発生回数
+    private int _countSuperIdleTime;
+    public int CountBad
+    {
+        get => _countBad;
+        set => _countBad = value;
+    }
+    public int CountGood 
+    {
+        get => _countGood; 
+        set => _countGood = value;
+    }
+    public int CountGreat
+    {
+        get => _countGreat;
+        set => _countGreat = value;
+    }
+    public float Distance
+    {
+        get => _distance; 
+        set => _distance = value;
+    }
+
+    public int CountSuperIdleTime
+    {
+        get => _countSuperIdleTime; 
+        set => _countSuperIdleTime = value;
+    }
+}
