894a46578805f28c05b024f1b7ac9ed271be9223
篠原 琉雅 <raitan3170@gmail.com>
2022-11-23 19:54:05 +0900

    インスペクターで表示する配列やリストの要素名を変更する機能を作成

--- Assets/Editor/ElementNamesDrawer.cs
+++ Assets/Editor/ElementNamesDrawer.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 using UnityEditor;
 
-/// <summary>SEの配列の各要素名を変更する為クラス </summary>
-[CustomPropertyDrawer(typeof(FlickSENames))]
-public class SEElementNameDrawer : PropertyDrawer
+// <summary>インスペクターで表示される配列やリストの要素名を変更する為クラス </summary>
+[CustomPropertyDrawer(typeof(ElementNames))]
+public class ElementNamesDrawer : PropertyDrawer
 {
-    /// <summary>要素名を変更 </summary>
+    //描画処理
     public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
     {
         try
         {
             int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
-            EditorGUI.PropertyField(rect, property, new GUIContent(((FlickSENames)attribute)._names[pos]));
+            EditorGUI.PropertyField(rect, property, new GUIContent(((ElementNames)attribute)._names[pos]));
 
         }
         catch
