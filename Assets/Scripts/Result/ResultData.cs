993f5c6c5559d4c9b5b31a520f45608d55de7b18
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2022-12-03 12:00:44 +0900

    ScriptableObjectの作成

--- Assets/Scripts/Result/ResultData.cs
+++ Assets/Scripts/Result/ResultData.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data",menuName ="ScriptableObjects/ResultSwitcherScriptableObject",order =1)]
 public class ResultData : ScriptableObject
 {
+    [SerializeField, Tooltip("難易度")]
+    string _modeName;
+    [SerializeField, Tooltip("Bad")]
+    int _bad;
+    [SerializeField, Tooltip("Good")]
+    int _good;
+    [SerializeField, Tooltip("Perfect")]
+    int _perfect;
     //TODO:各条件でのbad数,good数,perfect数のしきい値を定義する
 }
