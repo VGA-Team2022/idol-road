f0f10729b1f823b6853c2b426999a7ac581c3398
篠原 琉雅 <raitan3170@gmail.com>
2022-12-04 05:55:02 +0900

    一定数コンボが続いた時のイラストを表示する機能を修正（UIを使用した方法に変更）コンボイラストを表示するプレハブ（UI使用）を作成 #56

--- Assets/Scripts/InGame/InGameUIController.cs
+++ Assets/Scripts/InGame/InGameUIController.cs
@@ -3,45 +3,50 @@ using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>ゲームシーンで使用するUIの管理するクラス </summary>
-[RequireComponent(typeof(GameManager))]
 public class InGameUIController : MonoBehaviour
 {
-    [SerializeField, Header("ゴールまでの距離を表示するスライダー")]
+    [SerializeField, Header("コンボ時に表示するイラストの順番"), ElementNames(new string[] {"1", "2", "3"})]
+    Sprite[] _comboSprites = default;
+    [SerializeField, Tooltip("ゴールまでの距離を表示するスライダー")]
     Slider _goalSlider = default;
-    [SerializeField, Header("アイドルパワーを表示するスライダー")]
+    [SerializeField, Tooltip("アイドルパワーを表示するスライダー")]
     Slider _idolPowerGauge = default;
-    [SerializeField, Header("HPUIの親オブジェクト")]
+    [SerializeField, Tooltip("HPUIの親オブジェクト")]
     GridLayoutGroup _hpUIParent = default;
-    [SerializeField, Header("HPUIのプレハブ")]
+    [SerializeField, Tooltip("HPUIのプレハブ")]
     Image _hpPrefab = default;
-    [SerializeField, Header("コンボ数を表示するテキスト")]
+    [SerializeField, Tooltip("コンボ数を表示するテキスト")]
     TMP_Text _comboText = default;
+    [SerializeField, Tooltip("コンボイラストを表示するImage")]
+    Image _comboImage = default;
+    [SerializeField, Tooltip("コンボで使用するアニメーター"), ElementNames(new string[] {"背景", "イラスト"})]
+    Animator[] _comboAnimators = default;
+
     /// <summary>現在の表示しているHpUIの配列</summary>
     Image[] _currentHpUIArray = default;
-    GameManager _gameManager => GetComponent<GameManager>();
+    /// <summary>表示するイラストの添え字</summary>
+    int _currentComboIndex = 0;
 
-    private void Start()
+    /// <summary>InGameUIの初期化処理</summary>
+    /// <param name="maxHp">最大HP</param>
+    /// <param name="gameTime">ゲームプレイ時間</param>
+    /// <param name="maxIdlePower">最大アイドルパワー</param>
+    public void InitializeInGameUI(int maxHp, float gameTime, float maxIdlePower)
     {
-        _currentHpUIArray = new Image[_gameManager.MaxIdleHp];
+        _currentHpUIArray = new Image[maxHp];
 
-        for (var i = 0; i < _gameManager.MaxIdleHp; i++)    //HPUIを最大体力分表示する
+        for (var i = 0; i < maxHp; i++)    //HPUIを最大体力分表示する
         {
             HpUIGenerator(i);
         }
 
-        _goalSlider.maxValue = _gameManager.CountTime;          //スライダーの最大値をゲームプレイ時間と同じにする
-        _idolPowerGauge.maxValue = _gameManager.MaxIdlePower;   //アイドルパワーゲージ最大値を変更する
-
-        //関数を登録
-        _gameManager.OnReduceHpUI += ReduseHpUI;
-        _gameManager.OnChangeIdolPowerGauge += ChangeIdolPowerGauge;
-        _gameManager.OnUpdateComboText += UpdateComboText;
-        _gameManager.OnUpdateGoalDistanceUI += UpdateGoalDistanceUI;
+        _goalSlider.maxValue = gameTime;          //スライダーの最大値をゲームプレイ時間と同じにする
+        _idolPowerGauge.maxValue = maxIdlePower;   //アイドルパワーゲージ最大値を変更する
     }
 
     /// <summary>HPUIを生成し配列に追加する </summary>
     /// <param name="index">配列の添え字</param>
-    void HpUIGenerator(int index)
+    public void HpUIGenerator(int index)
     {
         var hpUI = Instantiate(_hpPrefab);
         var liveHaart = hpUI.transform.GetChild(0).GetComponent<Image>();   //子オブジェクトにある残ライフのイラストを取得する
@@ -49,33 +54,45 @@ public class InGameUIController : MonoBehaviour
 
         hpUI.transform.localScale = new Vector3(1, 1, 1);   //初期の大きさを決める
         liveHaart.rectTransform.sizeDelta = new Vector2(_hpUIParent.cellSize.x, _hpUIParent.cellSize.y);    //残ライフの大きさを調整する
-            
+
         _currentHpUIArray[index] = liveHaart;
     }
 
     /// <summary>現在の体力とHPUIを合わせる</summary>
     /// <param name="currentHp">現在の体力</param>
-    void ReduseHpUI(int currentHp)
+    public void UpdateHpUI(int currentHp)
     {
         _currentHpUIArray[currentHp].enabled = false;
     }
 
     /// <summary> アイドルパワーゲージを増減させる</summary>
     /// <param name="value">増減値(減少させる場合は負数を入れる)</param>
-    void ChangeIdolPowerGauge(int value)
+    public void UpdateIdolPowerGauge(int value)
     {
         _idolPowerGauge.value = value;
     }
 
     /// <summary>ゴールまでの距離を表示するUIを更新する</summary>
-    void UpdateGoalDistanceUI(float elapsedTime)
+    public void UpdateGoalDistanceUI(float elapsedTime)
     {
         _goalSlider.value = elapsedTime;   //ゴールまでの距離をUIに表示する
     }
 
     /// <summary>コンボ数を表示するテキストを更新する </summary>
-    void UpdateComboText(int comboCount)
+    public void UpdateComboText(int comboCount)
     {
         _comboText.text = comboCount.ToString();
+    }
+
+    /// <summary>コンボ用カットインのアニメーションを再生する</summary>
+    public void PlayComboAnimation()
+    {
+        _comboImage.sprite = _comboSprites[_currentComboIndex];     //イラストを変更
+        _currentComboIndex = (_currentComboIndex + 1) % _comboSprites.Length;   //イラストを循環させるため
+
+        foreach (var anim in _comboAnimators)
+        {
+            anim.SetTrigger("Play");
+        }
     }
 }
