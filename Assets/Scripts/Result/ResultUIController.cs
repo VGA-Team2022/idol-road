a54c48ec1fdf8f51fbfcc52b68bbc7b078ae0be7
篠原 琉雅 <raitan3170@gmail.com>
2022-12-03 09:53:53 +0900

    Result関連処理を管理するフォルダを作成

---
+++ Assets/Scripts/Result/ResultUIController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>リザルトシーンのUIを管理・更新するクラス</summary>
+public class ResultUIController : MonoBehaviour
+{
+    [SerializeField, Header("背景")]
+    Image _backGround = default;
+
+    /// <summary>0=神 1=良 2=普通 3=悪 </summary>
+    [SerializeField, Header("評価別背景"), ElementNames(new string[] {"神", "良", "普通", "悪"})]
+    Sprite[] _backGroundSprites = default;
+
+    /// <summary>結果によって背景を変更する </summary>
+    /// <param name="result"></param>
+    public void ChangeResultImage(Result result)
+    {
+        switch (result)
+        {
+            case Result.Bad:
+                _backGround.sprite = _backGroundSprites[3];
+                break;
+            case Result.Good:
+                _backGround.sprite = _backGroundSprites[2];
+                break;
+            case Result.Excellent:
+                _backGround.sprite = _backGroundSprites[1];
+                break;
+            case Result.Perfect:
+                _backGround.sprite = _backGroundSprites[0];
+                break;
+        }
+    }
+}
