65885340043ae851f08440e7219c92b7bde5a064
ryooout <ryota01010104@gmail.com>
2022-12-09 19:05:31 +0900

    セレクトシーンの作成 #140

---
+++ Assets/Scripts/StageSelect/StageSelectController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StageSelectController : MonoBehaviour
+{
+    [SerializeField, Header("ステージセレクトのボタン"), ElementNames(new string[] { "チュートリアル", "簡単", "普通", "難しい" })]
+    Button[] _stageSelectButton = default;
+    [SerializeField,Header("ステージセレクトによって変化するイメージ")]
+    Image _selectColor = default;
+    void Start()
+    {
+        //チュートリアルを押した時
+        _stageSelectButton[0].onClick.AddListener(() => _selectColor.color = Color.red);
+        //簡単を押した時
+        _stageSelectButton[1].onClick.AddListener(() => _selectColor.color = Color.blue);
+        //普通を押した時
+        _stageSelectButton[2].onClick.AddListener(() => _selectColor.color = Color.yellow);
+        //難しいを押した時
+        _stageSelectButton[3].onClick.AddListener(() => _selectColor.color = Color.green);
+    }
+}
