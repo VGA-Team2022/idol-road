456aa7186b7909feb0273e918df4d0e4ab389e83
RyuKento <83810925+RyuKento@users.noreply.github.com>
2022-12-09 17:30:06 +0900

    スーパーアイドルタイムでゲージによって出現するファンの制作 #43

--- Assets/Scripts/InGame/SuperIdolTime.cs
+++ Assets/Scripts/InGame/SuperIdolTime.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.Video;
 using System.Net;
+using Unity.Burst;
 
 public class SuperIdolTime : MonoBehaviour
 {
@@ -34,10 +35,24 @@ public class SuperIdolTime : MonoBehaviour
     private Image _fadePanel = default;
     [SerializeField,Tooltip("スーパーアイドルタイム画面のUIの親オブジェクト")]
     private GameObject _superIdolTimeObject = default;
+    [SerializeField, Tooltip("スーパーアイドルタイム画面の背景絵")]
+    private GameObject _backGroundPanel = default;
     [SerializeField, Tooltip("通常画面のCanvas")]
     private GameObject _normalCanvas = default;
     [SerializeField,Tooltip("通常時のプレイヤー")]
     private GameObject _normalPlayer = default;
+    [SerializeField, Tooltip("ゲージに合わせて出てくるファンの下段")]
+    private GameObject _BackDownFans = default;
+    [SerializeField, Tooltip("下段のファンが出てくる値")]
+    private float _downFansValue = 0.9f;
+    [SerializeField, Tooltip("ゲージに合わせて出てくるファンの中段")]
+    private GameObject _BackMiddleFans = default;
+    [SerializeField, Tooltip("中段のファンが出てくる値")]
+    private float _middleFansValue = 0.6f;
+    [SerializeField, Tooltip("ゲージに合わせて出てくるファンの上段")]
+    private GameObject _BackUpFans = default;
+    [SerializeField, Tooltip("上段のファンが出てくる値")]
+    private float _upFansValue = 0.3f;
     [SerializeField, Tooltip("背景に流すビデオのプレーヤー")]
     private VideoPlayer _videoPlayer = default;
     /// <summary>スーパーアイドルタイム中のタップされた回数</summary>
@@ -136,6 +151,9 @@ public class SuperIdolTime : MonoBehaviour
                 .OnComplete(() => { SwitchDisplayObject(); });
         isSuperIdolTime = false;
         //後ろのファンを飛ばす
+        _BackDownFans.GetComponent<Animator>().Play("Burst");
+        _BackMiddleFans.GetComponent<Animator>().Play("Burst");
+        _BackUpFans.GetComponent<Animator>().Play("Burst");
         this.gameObject.SetActive(false);
     }
     public void SwitchDisplayObject()
@@ -155,26 +173,30 @@ public class SuperIdolTime : MonoBehaviour
             sequence.Append(_imageGauge.DOFillAmount(_gaugeLength, _gaugePlusTime));
             //GaugeAdvance(_gaugeLength);
         }
-        if (_gaugeLength > 33.3f && !isDownEnemy)
+        if (_gaugeLength > _downFansValue && isDownEnemy == false)
         {
             isDownEnemy = true;
+            _BackDownFans.GetComponent<Animator>().Play("SlideIn");
             Debug.Log("Down");
         }
-        if (_gaugeLength > 66.6f && !isMiddleEnemy)
+        if (_gaugeLength > _middleFansValue && isMiddleEnemy == false)
         {
             isMiddleEnemy = true;
+            _BackMiddleFans.GetComponent<Animator>().Play("SlideIn");
             Debug.Log("Middle");
+        }
+        if (_gaugeLength > _upFansValue && isUpEnemy == false)
+        {
+            isUpEnemy = true;
+            _BackUpFans.GetComponent<Animator>().Play("SlideIn");
+            Debug.Log("Up");
         }
         if (_gaugeLength > 1)
         {
             _gaugeLength = 1;
             _isGaugeMax = true;
             //Debug.Log("Full");
-            if (!isUpEnemy)
-            {
-                isUpEnemy = true;
-                Debug.Log("Up");
-            }
+            
         }
         //Debug.Log($"max:{_gaugeCountMax},count:{_gaugeCount},gauge:{_gaugeLength}");
     }
@@ -183,6 +205,7 @@ public class SuperIdolTime : MonoBehaviour
         _superIdolTimeObject.SetActive(true);
         isSuperIdolTime = true;
         _videoPlayer.gameObject.SetActive(false);
-        Debug.Log("endvideo");
+        _backGroundPanel.gameObject.SetActive(true);
+        //Debug.Log("endvideo");
     }
 }
