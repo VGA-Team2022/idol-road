1dc3c313a6074eac06e8e2f1489799ee015c2fc8
篠原 琉雅 <raitan3170@gmail.com>
2022-12-15 06:05:39 +0900

    壁ファンの爆発演出を作成 #79

--- Assets/Scripts/Enemy/WallEnemy.cs
+++ Assets/Scripts/Enemy/WallEnemy.cs
@@ -5,6 +5,9 @@ using DG.Tweening;
 /// <summary>壁になるファンの処理を行うクラス</summary>
 public class WallEnemy : EnemyBase
 {
+    /// <summary>吹き飛び用アニメーションコントローラー </summary>
+    Animator _anim => GetComponent<Animator>();
+
     protected override void BadEffect()
     {
         //横移動
@@ -21,8 +24,13 @@ public class WallEnemy : EnemyBase
 
     protected override void GoodEffect()
     {
-        Destroy(gameObject);
-        // throw new System.NotImplementedException();
+        _anim.Play("Effect");
+        Array.ForEach(EnemySprites, e =>
+        {
+            e.transform.DOLocalRotate(new Vector3(0, 0, 360f), 0.1f, RotateMode.FastBeyond360)
+           .SetEase(Ease.Linear)
+           .SetLoops(-1, LoopType.Restart);
+        });
     }
 
     protected override void OutEffect()
@@ -33,7 +41,21 @@ public class WallEnemy : EnemyBase
 
     protected override void PerfactEffect()
     {
-        Destroy(gameObject);
-        // throw new System.NotImplementedException();
+        _anim.Play("Enemy");
+        Array.ForEach(EnemySprites, e =>
+        {
+            e.transform.DOLocalRotate(new Vector3(0, 0, 360f), 0.1f, RotateMode.FastBeyond360)
+           .SetEase(Ease.Linear)
+           .SetLoops(-1, LoopType.Restart);
+        });
+    }
+
+    /// <summary>
+    /// 吹き飛びアニメーションが終了したら自身を削除する
+    /// アニメーショントリガーで呼び出す
+    /// </summary>
+    public void ThisDestroy()
+    {
+        Destroy(this.gameObject);
     }
 }
