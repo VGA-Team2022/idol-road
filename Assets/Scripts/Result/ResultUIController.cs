dc325d8ba306161761b9b3eeadb4d5242922dd66
ryooout <ryota01010104@gmail.com>
2022-12-17 17:11:51 +0900

    ファンのアニメーションの微調整。リザルト時に評価画面とファンのチャット画面の切り替えを行うことが可能になった。

--- Assets/Scripts/Result/ResultUIController.cs
+++ Assets/Scripts/Result/ResultUIController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using DG.Tweening;
 using TMPro;
 /// <summary>リザルトシーンのUIを管理・更新するクラス</summary>
@@ -14,18 +15,27 @@ public class ResultUIController : MonoBehaviour
     Sprite[] _backGroundSprites = default;
     [SerializeField, Header("結果を表示するテキスト"), ElementNames(new string[] { "評価","Bad", "Good", "Perfect", "合計スコア"})]
     TextMeshProUGUI[] _resultValueText = default;
+    [SerializeField, Header("リザルト表示切替"),ElementNames(new string[] { "評価","みんなのコメント"})]
+    Transform[] _showResultParent = default;
+    [SerializeField, Header("フェードイン関連"),Tooltip("ボタンイメージ"),ElementNames(new string[] { "評価切り替え", "ステージセレクト","リトライ" })]
+    Image[] _fadeImageButton = default;
+    [SerializeField, Tooltip("テキスト"), ElementNames(new string[] { "評価切り替え", "ステージセレクト", "リトライ" })]
+    TextMeshProUGUI[]_fadeTextColor = default;
     [SerializeField,Tooltip("値の増加時間")]
     float _increseTime = 1.0f;
-    [SerializeField, Tooltip("リザルトテキストを表示させるまでの時間")]
-    float _showResultSpan = 1.0f,_scoreResultSpan = 2.0f;
+    [SerializeField, Tooltip("UIを表示させるまでの時間"),ElementNames(new string[] {"評価表示","スコア表示","ボタンのフェードイン","テキストのフェードイン"})]
+    float _showResultSpan = 1.0f,_scoreResultSpan = 2.0f,_buttonShowSpan = 1.0f,_textShowSpan = 2.0f;
     [SerializeField, Tooltip("リザルト評価それぞれのカウントとスコア")]
     int _countBad, _countGood, _countPerfect,_countScore;
+    [Header("評価画面が表示されているかどうか")]
+    bool _isValue;
     ResultManager _result;
     [SerializeField]
     ResultSwitcher _reSwitch;
     private void Start()
     {
         _result = ResultManager.Instance;
+        _isValue = true;
         StartCoroutine(ShowResult());
     }
 
@@ -73,6 +83,41 @@ public class ResultUIController : MonoBehaviour
         _resultValueText[4].gameObject.SetActive(true);
         DOTween.To(() => _countScore, (r) => _countScore = r,/*_reSwitch.Score*/1000, _increseTime)
              .OnUpdate(() => _resultValueText[4].text = $"Score:{ /*_reSwitch.Score */_countScore}");
+        yield return new WaitForSeconds(_scoreResultSpan);
+        for(int i = 0;i<_fadeImageButton.Length;i++)
+        {
+            _fadeImageButton[i].gameObject.SetActive(true);
+            _fadeImageButton[i].DOFade(1.0f, _buttonShowSpan);
+            _fadeTextColor[i].DOFade(1.0f, _textShowSpan);
+        }
         yield return null;
+    }
+    /// <summary>リザルトUIを切り替える</summary>
+    public void SwitchResultUi()
+    {
+        //ボタンを押したときにValueが表示されていたら
+        if(_isValue)
+        {
+            //評価画面隠す
+            _showResultParent[0].gameObject.SetActive(false);
+            //みんなのコメントを表示
+            _showResultParent[1].gameObject.SetActive(true);
+            _isValue = false;
+        }
+        //されていなかったら
+        else if(!_isValue)
+        {
+            //評価画面を出す
+            _showResultParent[0].gameObject.SetActive(true);
+            //みんなのコメントを非表示に
+            _showResultParent[1].gameObject.SetActive(false);
+            _isValue = true;
+        }
+    }
+    /// <summary>難易度セレクトシーンに戻るかリトライするか</summary>
+    /// <param name="index">シーン番号</param>
+    public void ReturnModeSelectAndRetry(int index)
+    {
+        SceneManager.LoadScene(index);
     }
 }
