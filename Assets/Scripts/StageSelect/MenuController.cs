b06c4b80b7f0b3cebb2970a021d8e5e1b20e6b68
篠原 琉雅 <raitan3170@gmail.com>
2022-12-27 17:56:17 +0900

    クレジットを表示する機能をメニューに追加 #266

--- Assets/Scripts/StageSelect/MenuController.cs
+++ Assets/Scripts/StageSelect/MenuController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class MenuController : MonoBehaviour
 {
+    /// <summary>各UIを表示する為のアニメーション名 </summary>
+    const string OPEN_UI_ANIME_NAME = "Open";
+
+    /// <summary>各UIを非表示する為のアニメーション名 </summary>
+    const string CLOSE_UI_ANIME_NAME = "Close";
+
     [SerializeField, Header("チュートリアル用アニメーター")]
     Animator _tutorialAnimator = default;
 
     [SerializeField, Header("クレジット用アニメーター")]
     Animator _creditAnimator = default;
 
-    /// <summary>メニュー用アニメーター </summary>
-    Animator _menuAnim =>GetComponent<Animator>();
-
+    [SerializeField, Header("メニュー用アニメーター")]
+    Animator _menuAnim = default;
+ 
     /// <summary>メニューUIを操作する関数 </summary>
     /// <param name="flag">true=表示 false=非表示</param>
     public void MenuOperator(bool flag)
@@ -20,11 +26,11 @@ public class MenuController : MonoBehaviour
         if (flag)   
         {
 
-            _menuAnim.Play("MenuOpen");          //メニューを表示
+            _menuAnim.Play(OPEN_UI_ANIME_NAME);          //メニューを表示
         }
         else
         {
-            _menuAnim.Play("MenuClose");        //メニューを非表示
+            _menuAnim.Play(CLOSE_UI_ANIME_NAME);        //メニューを非表示
         }
 
         AudioManager.Instance.PlaySE(7);　
@@ -37,12 +43,12 @@ public class MenuController : MonoBehaviour
     {
         if (flag)
         {
-            _tutorialAnimator.Play("TutorialOpen");
+            _tutorialAnimator.Play(OPEN_UI_ANIME_NAME);
             
         }
         else
         {
-            _tutorialAnimator.Play("TutorialClose");
+            _tutorialAnimator.Play(CLOSE_UI_ANIME_NAME);
            
         }
 
@@ -55,11 +61,11 @@ public class MenuController : MonoBehaviour
     {
         if (flag)
         {
-            _creditAnimator.Play("CreditOpen");
+            _creditAnimator.Play(OPEN_UI_ANIME_NAME);
         }
         else
         {
-            _creditAnimator.Play("CreditClose");
+            _creditAnimator.Play(CLOSE_UI_ANIME_NAME);
         }
 
         AudioManager.Instance.PlaySE(7);　
