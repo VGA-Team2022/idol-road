3cc3bf3520314379da327e9dca8bd5e17c5a9376
篠原 琉雅 <raitan3170@gmail.com>
2023-01-01 15:29:03 +0900

    ボスステージ用 通常ファンパラメーターを設定するscriptableobjectを作成 #249

--- Assets/Scripts/GameParameter/BossStageParameter.cs
+++ Assets/Scripts/GameParameter/BossStageParameter.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-/// <summary>エネミーのパラメーターを設定するクラス </summary>
+/// <summary>ボスステージでの通常ファンパラメーター </summary>
 [CreateAssetMenu]
-public class EnemyParameter : ScriptableObject
+public class BossStageParameter : ScriptableObject
 {
-    [SerializeField, Header("----------------------------通常時-------------------------------"),Header("与えるダメージ")]
+    [SerializeField, Header("----------------------------ボス戦の時----------------------------"), Header("与えるダメージ")]
     int _addDamageValue = 0;
 
     [SerializeField, Header("向かってくる速度")]
@@ -16,9 +16,9 @@ public class EnemyParameter : ScriptableObject
     [SerializeField, Header("獲得出来るアイドルパワー量")]
     int _addIdolPowerValue = 0;
 
-    [SerializeField, Header("リズム判定の秒数"), ElementNames(new string[] {"合計時間", "Bad", "Good", "Perfect", "Out"})]
+    [SerializeField, Header("リズム判定の秒数"), ElementNames(new string[] { "合計時間", "Bad", "Good", "Perfect", "Out" })]
     float[] _rhythmTimes = new float[5];
-        
+
     /// <summary>向かってくる速度</summary>
     public float MoveSpped => _moveSpeed;
 
@@ -27,10 +27,10 @@ public class EnemyParameter : ScriptableObject
 
     /// <summary>リズム判定の秒数 0=合計時間 1=Bad 2=Good 3=Perfect 4=Out</summary>
     public float[] RhythmTimes => _rhythmTimes;
-   
+
     /// <summary>与えるダメージ </summary>
     public int AddDamageValue => _addDamageValue;
-   
+
     /// <summary>獲得できるアイドルパワー量</summary>
     public int AddIdolPowerValue => _addIdolPowerValue;
 }
