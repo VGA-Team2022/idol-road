34aa1c90f1d1313abacfadd46ee8a4de7e85eb18
篠原 琉雅 <raitan3170@gmail.com>
2023-01-04 01:19:20 +0900

    ストーリー表示方法を変更 #267

--- Assets/Scripts/Story/StoryPrinter.cs
+++ Assets/Scripts/Story/StoryPrinter.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>ストーリーを表示する為のクラス </summary>
+[RequireComponent(typeof(Animator))]
 public class StoryPrinter : MonoBehaviour
 {
     [SerializeField, Header("テキストを表示するまでにかかる秒数")]
@@ -12,21 +15,30 @@ public class StoryPrinter : MonoBehaviour
     float _textWaitTime = 2f;
     [SerializeField, Header("フェードを行うまでの待機時間")]
     float _fadeWaitTime = 2f;
-
-    [SerializeField, Tooltip("フェード行うオブジェクト")]
-    FadeController _fadeController = default;
-
     [SerializeField, Tooltip("表示するストーリーのテキスト配列")]
     TMP_Text[] _storyTexts = default;
+    [SerializeField, Tooltip("キャンバスを閉じる為のボタン")]
+    Button _closeButton = default;
+
     /// <summary>表示したテキストの添え字 </summary>
     int _currnetTextIndex = 0;
+    /// <summary>ストーリー表示のコルーチンを終了させる為の変数 </summary>
+    IEnumerator _showStroyEnumerator = default;
 
-    /// <summary>シーン遷移を行ったかどうか </summary>
-    bool _isTransition = false;
+    Animator _anim => GetComponent<Animator>();
+    TMP_Text _closeButtonText => _closeButton.transform.GetChild(0).GetComponent<TMP_Text>();
 
-    private void OnEnable()
+    /// <summary>
+    /// アニメーションさせた値をリセットする
+    /// 再度アニメーションさせる為
+    /// </summary>
+    void ResetValue()
     {
-        StartCoroutine(ShowStory());
+        Array.ForEach(_storyTexts, s => s.alpha = 0);
+        _closeButton.gameObject.SetActive(false);
+        _closeButtonText.alpha = 0;
+        _closeButton.image.color = new Color(1, 1, 1, 0);
+        _currnetTextIndex = 0;
     }
 
     /// <summary>ストーリーを表示する </summary>
@@ -40,15 +52,9 @@ public class StoryPrinter : MonoBehaviour
 
         yield return new WaitForSeconds(_fadeWaitTime);
 
-        if (!_isTransition)
-        {
-            _fadeController.FadeOut(() =>
-            {
-                gameObject.SetActive(false);
-                _fadeController.FadeIn();
-            });
-            _isTransition = true;
-        }
+        _closeButton.gameObject.SetActive(true);
+        _closeButton.image.DOFade(1, 2);
+        _closeButtonText.DOFade(1, 2);
     }
 
     /// <summary>テキストをフェードさせて表示する </summary>
@@ -59,21 +65,37 @@ public class StoryPrinter : MonoBehaviour
     }
 
     /// <summary>
-    /// スキップ機能
+    /// ストーリーUIを操作する
+    /// ボタンから呼び出す
+    /// </summary>
+    /// <param name="flag">true=表示 false=非表示</param>
+    public void StroyOperator(bool flag)
+    {
+        if (flag)
+        {
+            _anim.Play("Open");
+            _showStroyEnumerator = ShowStory();
+            StartCoroutine(_showStroyEnumerator);
+        }
+        else
+        {
+            _anim.Play("Close");
+            ResetValue();
+        }
+
+        AudioManager.Instance.PlaySE(7);
+    }
+
+    /// <summary>
+    /// すべてのテキストを一気に表示する
     /// ボタンから呼び出す
     /// </summary>
     public void Skip()
     {
-        if (_isTransition) { return; }
-
-        _fadeController.FadeOut(() =>
-        {
-            gameObject.SetActive(false);
-            _fadeController.FadeIn();
-        });
-
-        _isTransition = true;
+        _closeButton.gameObject.SetActive(true);
+        Array.ForEach(_storyTexts, s => s.alpha = 1);
+        _closeButtonText.alpha = 1;
+        _closeButton.image.color = new Color(1, 1, 1, 1);
+        StopCoroutine(_showStroyEnumerator);
     }
-
-
 }
